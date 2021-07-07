fff316c97b9aa5a788febe3341430ce01e708d8e
tengyu <tengyu@tencent.com>
2021-07-07 15:11:30 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'MrSlimSlim',
+  description: 'MrSlimSlim Blog',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@img': '../../assets/images'
+      }
+    }
+  }
+}
